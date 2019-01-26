bce512985913bd0c2e4d37e37669b9b50cd19c57
huangwenxi <389125903@qq.com>
2019-01-26 16:29:41 +0800

    实现了二叉查找树的删除，找最大最小节点的操作

--- binarytree/btree.c
+++ binarytree/btree.c
@@ -32,6 +32,112 @@ btree insert(unsigned int x, btree t)
 	return t;
 }
 /*
+ *返回值有以下两种情况：
+ 1.如果t没有右儿子则返回t
+ 2.如果t有右儿子则返回t中element最大的节点的父节点
+ * */
+btree findmaxnodeparent(btree t)
+{
+	btree tmp = NULL;
+	btree x=NULL,p=NULL;
+	//t为空树
+	if(t==NULL)
+		return NULL;
+	tmp = t;
+	//如果t没有右子树，直接返回t
+	if(tmp->right==NULL)
+		return tmp;
+	//t有右子树
+	p = tmp;
+	x = p->right;
+	//判断x是否为最右的节点了
+	while(x->right)
+	{
+		p = p->right;
+		x = x->right;
+	}
+	//此时x是t中最大的节点,p是x的父节点
+	return p;
+}
+/*
+ *从以t为根的二叉查找树中删除element为x的节点
+ * */
+btree delete(unsigned int x, btree t)
+{
+	btree p=NULL, x_=NULL;//没想好合适的命名。。。
+	if(t == NULL)
+		return NULL;
+	//t为待删除的节点的指针
+	if(x == t->element)
+	{
+		//删除的节点无左子树，则直接删除节点t，且将t赋值为t的右子树的根节点的指针。
+		if(t->left == NULL)
+		{
+			t = t->right;
+			return t;
+		}
+		//t有左子树的情况
+		p = findmaxnodeparent(t->left);
+		//如果t的左子树没有右儿子
+		if(p->right==NULL)
+		{
+			p->right = t->right;
+			t = p;
+			return t;
+		}
+		//t的左子树有右儿子的情形
+		else if(p->right != NULL)
+		{
+			//保存准备替换到删除节点t位置的节点x信息
+			x_ = p->right;
+			//将p的右子树设置为x的左子树
+			p->right = p->right->left;
+			//将x的左子树设置为t的左子树
+			x_->left = t->left;
+			//将x的右子树设置为t的右子树
+			x_->right = t->right;
+			//t设置成x返回
+			t = x_;
+			return t;
+			
+		}
+
+	}
+	//没有找到element为x的节点则继续遍历
+	else if(x<t->element)
+		t->left = delete(x, t->left);
+	else 
+		t->right = delete(x, t->right);
+
+	return t;
+}
+/*
+ *寻找二叉查找树t中最大的节点
+ * */
+btree findmax(btree t)
+{
+	if(t == NULL)
+		return NULL;
+	while(t->right)
+	{
+		t = t->right;		
+	}
+	return t;
+}
+/*
+ *寻找二叉查找树t中最小的节点
+ * */
+btree findmin(btree t)
+{
+	if(t == NULL)
+		return NULL;
+	while(t->left)
+	{
+		t = t->left;
+	}
+	return t;
+}
+/*
  *t:指向树的根节点
  *func:找到以t为根节点的树中的所有节点的数量
  * */
@@ -95,9 +201,26 @@ int main()
 	t = insert(6, t);
 	t = insert(3, t);	
 	t = insert(7, t);
-	t = insert(6, t);
 	print(t);printf("\n");
-	printf("number of node=%d\n", findnumberofnode(t));
-	printf("number of leaf node= %d\n", findnumberofleafnode(t));
-	printf("number of full node=%d\n", findnumberoffullnode(t));
+/*	t = delete(6, t);
+	print(t);printf("\n");
+	t = delete(3, t);
+	print(t);printf("\n");
+	t = delete(1, t);
+	print(t);printf("\n");
+	t = delete(7, t);
+	print(t);printf("\n");
+	if(t==NULL)
+	{	
+		printf("t is a empty tree\n");
+		return 0;
+	}*/
+	if(t != NULL)
+	{
+		printf("the max node's element=%d\n",findmax(t)->element);
+		printf("the min node's element=%d\n",findmin(t)->element);
+	}
+	//printf("number of node=%d\n", findnumberofnode(t));
+	//printf("number of leaf node= %d\n", findnumberofleafnode(t));
+	//printf("number of full node=%d\n", findnumberoffullnode(t));
 }
